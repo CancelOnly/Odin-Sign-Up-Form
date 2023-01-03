283041193aad150c06f591c06684044630246637
CancelOnly <70231421+CancelOnly@users.noreply.github.com>
2023-01-03 05:51:40 -0300

    fixed password no characters

--- index.js
+++ index.js
@@ -4,22 +4,25 @@ function checkPassword() {
     let cpasswordBorder = document.getElementById("cnfrm_password");
     let cnfrmPassword = document.getElementById("cnfrm_password").value;
     let message = document.getElementById("message");
-    
+
     if (password.length != 0) {
         if (password == cnfrmPassword) {
             passwordBorder.style.borderColor = "green";
             cpasswordBorder.style.borderColor = "green";
-            message.textContent = "*";
+            message.textContent = "Password match";
             message.style.color = "black";
         } else {
-            message.textContent = "*Password don't match";
+            message.textContent = "Password don't match";
             passwordBorder.style.borderColor = "red";
             cpasswordBorder.style.borderColor = "red";
             message.style.color = "red";
         }
     }
-    
+
     else {
-        message.textContent = "*";
+        passwordBorder.style.borderColor = "red";
+        cpasswordBorder.style.borderColor = "red";
+        message.style.color = "red";
+        message.textContent = "Need at least 1 character";
     }
 }
